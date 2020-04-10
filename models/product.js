8d5f554ad83a8b538a4a055e9ca66b73e644b003
Prashantk751 <Prashantk751@gmail.com>
2020-04-10 22:38:07 +0530

    dynamic routes changes

--- models/product.js
+++ models/product.js
@@ -1,7 +1,7 @@
 //const products=[];
 const fs = require('fs');
 const path = require('path');
-
+const Cart = require('./cart');
 const p = path.join(
     path.dirname(process.mainModule.filename),
     'data',
@@ -23,22 +23,47 @@ const getProductsFromFile = cb => {
 };
 
 module.exports = class Product{
-    constructor(title, imageUrl, description, price)
+    constructor(id, title, imageUrl, description, price)
     {
+        this.id = id;
         this.title = title;
         this.imageUrl = imageUrl;
         this.description = description;
         this.price = price;
     }
 
+    static deleteById(id) {
+        getProductsFromFile(products => {
+          const product = products.find(prod => prod.id === id);
+          const updatedProducts = products.filter(prod => prod.id !== id);
+          fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+            if (!err) {
+              Cart.deleteProduct(id, product.price);
+            }
+          });
+        });
+      }
+
     save(){
-        //products.push(this)
+       
        getProductsFromFile(products => {
-        products.push(this);
-        fs.writeFile(p, JSON.stringify(products),(err) => {
+
+        if(this.id){
+            const existingProductIndex = products.findIndex(prod => prod.id === this.id);
+            const updatedProducts = [...products];
+            updatedProducts[existingProductIndex] = this;
+
+            fs.writeFile(p, JSON.stringify(products),(err) => {
+                console.log(err);
+            });
+        }else{
+            //products.push(this)
+             this.id = Math.random().toString();
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products),(err) => {
             console.log(err);
         });
-
+        }
        });    
     }
 
@@ -47,4 +72,11 @@ module.exports = class Product{
         //return products;
         getProductsFromFile(cb);
     }
+
+   static findById(id, cb){
+       getProductsFromFile(products =>{
+           const product = products.find( p => p.id === id);
+           cb(product);
+       })
+   } 
 }
--- routes/admin.js
+++ routes/admin.js
@@ -10,5 +10,11 @@ router.get('/admin/products', adminController.getProducts);
 
 router.post('/admin/add-product', adminController.postAddProduct);
 
+router.get('/admin/edit-product/:productId', adminController.getEditProduct);
+
+router.post('/admin/edit-product', adminController.postEditProduct);
+
+router.post('/admin/delete-product', adminController.postDeleteProduct);
+
 module.exports = router;
 //exports.routes = router;
